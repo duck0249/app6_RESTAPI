8471e6e82897757f73a89ab95475b52393a66e30
duck0249 <donggeunlee@Donggeuns-MacBook-Air.local>
2024-10-15 09:32:47 +0700

    first flask function build

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-## App6 : REST API, Create REST API.
+## App6 : REST API, Create REST API powered by Flask
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    try:
+        return render_template("home.html")
+    except Exception as e:
+        return f"Error: {e}"
+
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    try:
+        # df = pandas.read_csv("")
+        temperature = 23
+        return {"station": station,
+        		"date": date,
+        		"temperature": temperature}
+
+    except Exception as e:
+        return f"Error: {e}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
