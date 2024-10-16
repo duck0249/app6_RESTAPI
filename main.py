abd768780517e68248fb28b412e23dbc358305e2
duck0249 <donggeunlee@Donggeuns-MacBook-Air.local>
2024-10-16 09:16:10 +0700

    REST API created

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 ## App6 : REST API, Create REST API powered by Flask
 from flask import Flask, render_template
-import requests
+# import requests
 import pandas as pd
+from datetime import datetime
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
@@ -17,8 +17,13 @@ def home():
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     try:
-        # df = pandas.read_csv("")
-        temperature = 23
+        filename = f"./data_small/TG_STAID{str(station).zfill(6)}.txt"
+        df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+
+        date = datetime.strptime(date, "%Y%m%d")
+        date = date.strftime("%Y-%m-%d")
+
+        temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
         return {"station": station,
         		"date": date,
         		"temperature": temperature}
