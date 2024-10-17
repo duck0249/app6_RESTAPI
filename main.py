f7dbcfea9c2fcb87076cfcaec3869f791181c0a3
duck0249 <donggeunlee@Donggeuns-MacBook-Air.local>
2024-10-17 08:49:49 +0700

    final code

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ df_station = pd.read_csv("./data_small/stations.txt", skiprows=17)
 df_station = df_station.iloc[:20,[0,1]]
 df_html = df_station.to_html(header=True)
 
+
 @app.route("/")
 def home():
     try:
@@ -17,16 +18,13 @@ def home():
 
 
 @app.route("/api/v1/<station>/<date>")
-def about(station, date):
+def api_all(station, date):
     try:
         filename = f"./data_small/TG_STAID{str(station).zfill(6)}.txt"
         df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
 
         date = datetime.strptime(date, "%Y%m%d")
         date = date.strftime("%Y-%m-%d")
-
-        
-
         temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
         return {"station": station,
         		"date": date,
@@ -35,6 +33,32 @@ def about(station, date):
     except Exception as e:
         return f"Error: {e}"
 
+
+@app.route("/api/v1/<station>/")
+def api_station(station):
+	try:
+		filename = f"./data_small/TG_STAID{str(station).zfill(6)}.txt"
+		df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+		result = df.to_dict(orient="records")
+
+		return result
+
+	except Exception as e:
+		return f"Error: {e}"
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def api_yearly(station, year):
+	try:
+		filename = f"./data_small/TG_STAID{str(station).zfill(6)}.txt"
+		df = pd.read_csv(filename, skiprows=20)
+		df["    DATE"] = df["    DATE"].astype(str)
+		result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+
+		return result
+	
+	except Exception as e:
+		return f"Error : {e}"
 
 if __name__ == "__main__":
     app.run(debug=True)
